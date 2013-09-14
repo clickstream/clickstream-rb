2505a9e4746bdd65e9967c6ff18a82ca6af4a540
Jerome Touffe-Blin <jtouffeblin@expedia.com>
2013-09-14 11:01:42 +1000

    Return time in addition to response for logging and use localhost for testing

--- lib/columbo/api_client.rb
+++ lib/columbo/api_client.rb
@@ -4,7 +4,8 @@ require 'json'
 module Columbo
   class APIClient
 
-    API_URI     = "http://localhost:15080/hit".freeze
+    API_URI     = "http://localhost:15080/capture".freeze
+    #API_URI     = "http://columbo.aws.af.cm/capture".freeze
 
     def initialize(api_key, api_uri)
       @api_key = api_key
@@ -25,9 +26,14 @@ module Columbo
       payload = zlib.deflate(json)
 
       uri = URI(@api_uri)
-      Net::HTTP.start(uri.host, uri.port) do |http|
+
+      start = Time.now
+      Net::HTTP.new(uri.host, uri.port).start do |http|
         response = http.post(@api_uri, payload, headers)
-        zlib.unzip(response.body, response['Content-Encoding'])
+
+        stop = Time.now
+        duration = ((stop-start) * 1000).round(3)
+        zlib.unzip(response.body, response['Content-Encoding']) + ' - Time: ' + duration.to_s + 'ms'
       end
     end
 
